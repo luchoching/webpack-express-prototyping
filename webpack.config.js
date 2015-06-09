8d55ca85bcaed39c6fe1ff5843652adffd47adea
Lu Cho-Ching <choching.work@gmai.com>
2015-06-09 14:25:10 +0800

    misc fix

--- webpack.config.js
+++ webpack.config.js
@@ -7,48 +7,49 @@ var mainPath = path.resolve(__dirname, 'app', 'main.js');
 
 
 var config = {
-  devtool: 'eval',
-  //the base path which will be used to resolve entry points
-  context: __dirname,
-  entry: [
-    'webpack/hot/dev-server',
-    'webpack-dev-server/client?http://localhost:8080',
-    mainPath
-  ],
-  output: {
-    path: buildPath,
-    filename: 'bundle.js',
-    publicPath: '/build'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/, 
-        loader: 'babel', 
-        exclude: [nodeModulePath]
-      },
-      { 
-        test: /\.css$/, 
-        loader: 'style!css' 
-      },
-      { 
-        test: /\.scss$/, 
-        loader: 'style!css!sass' 
-      },
-      { 
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
-        loader: 'url-loader?limit=100000' 
-      }
-    ]
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: 'jquery',
-      _: "lodash"
-    })
-  ]
+  devtool: 'eval'
 };
 
+config.entry= [
+  'webpack/hot/dev-server',
+  'webpack-dev-server/client?http://localhost:8080',
+  mainPath
+];
+
+config.output= {
+  path: buildPath,
+  filename: 'bundle.js',
+  publicPath: '/build'
+};
+
+config.module = {};
+config.module.loaders= [
+  {
+    test: /\.js$/, 
+    loader: 'babel', 
+    exclude: [nodeModulePath]
+  },
+  { 
+    test: /\.css$/, 
+    loader: 'style!css' 
+  },
+  { 
+    test: /\.scss$/, 
+    loader: 'style!css!sass' 
+  },
+  { 
+    test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
+    loader: 'url-loader?limit=100000' 
+  }
+];
+
+config.plugins= [
+  new webpack.HotModuleReplacementPlugin(),
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: 'jquery',
+    _: "lodash"
+  })
+];
+ 
 module.exports = config;
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -5,41 +5,42 @@ var buildPath = path.resolve(__dirname, 'public', 'build');
 var mainPath = path.resolve(__dirname, 'app', 'main.js');
 
 var config = {
-  // we change to normal source mapping
-  devtool: 'source-map',
-  entry: mainPath,
-  output: {
-    path: buildPath,
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/, 
-        loader: 'babel', 
-        exclude: [nodeModulePath]
-      },
-      { 
-        test: /\.css$/, 
-        loader: 'style!css' 
-      },
-      { 
-        test: /\.scss$/, 
-        loader: 'style!css!sass' 
-      },
-      { 
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
-        loader: 'url-loader?limit=100000' 
-      }
-    ]
-  },
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: 'jquery',
-      _: "lodash"
-    })
-  ]
+  devtool: 'source-map'
 };
+
+config.entry= mainPath;
+
+config.output= {
+  path: buildPath,
+  filename: 'bundle.js'
+};
+
+config.module = {};
+config.module.loaders= [
+  {
+    test: /\.js$/, 
+    loader: 'babel', 
+    exclude: [nodeModulePath]
+  },
+  { 
+    test: /\.css$/, 
+    loader: 'style!css' 
+  },
+  { 
+    test: /\.scss$/, 
+    loader: 'style!css!sass' 
+  },
+  { 
+    test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
+    loader: 'url-loader?limit=100000' 
+  }
+];
+config.plugins= [
+  new webpack.ProvidePlugin({
+    $: "jquery",
+    jQuery: 'jquery',
+    _: "lodash"
+  })
+];
 
 module.exports = config;
