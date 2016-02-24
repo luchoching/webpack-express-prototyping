8a401ae0321b417613104a9fe6907aa3fe3915e8
Lu Cho-Ching <choching.work@gmail.com>
2016-02-24 17:49:16 +0800

    Update package version

--- app/components/Hello.jsx
+++ app/components/Hello.jsx
@@ -1,9 +1,19 @@
-var Hello = React.createClass({
-  render: function(){
-    return (
-      <h1>Hello 中文 順耶!</h1>
-    );
+/*global React b:true*/
+class Hello extends React.Component {
+  constructor() {
+    super();
+    this._handleClick = this._handleClick.bind(this);
+    this.state = {
+      say: 'hi'
+    };
   }
-});
+  render() {
+   return <h2 onClick={this._handleClick}>dHello ES6 !!!! </h2>;
+
+  }
+  _handleClick() {
+    console.log(this.state.say);
+  }
+}
 
 module.exports = Hello;
